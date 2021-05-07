c90df37c0b5442a0b110a3865fedadc37af297bd
Randy Faison <randyfaison@randys-mbp.myfiosgateway.com>
2021-05-07 08:42:38 -0400

    added nodemon to constantly watch server for changed

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+//red notes from db.json
+let notes = fs.readFileSync('./db/db.json');
+notes = JSON.parse(notes)
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+
+//home route
+
+
+app.listen(PORT, () => console.log(`server started on http://localhost:${PORT}`));
